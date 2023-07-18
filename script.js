b1734b6e7e083d160c56254aa7a27223160d6eb7
superfishal <97133716+superfishal@users.noreply.github.com>
2023-07-17 18:44:20 -0700

    API connected, model instatiated and inquirer able to receive prompts

---
+++ script.js
@@ -0,0 +1,39 @@
+// dependencies
+const { OpenAI } = require("langchain/llms/openai");
+const inquirer = require("inquirer");
+require("dotenv").config();
+
+// Creates and stores a wrapper for the OpenAI package along with basic configuration
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  model: "gpt-3.5-turbo",
+});
+
+// Uses the instantiated OpenAI wrapper, model, and makes a call based on input from inquirer
+const promptFunc = async (input) => {
+  try {
+    const res = await model.call(input);
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Initialization function that uses inquirer to prompt the user and returns a promise. It takes the user input and passes it through the call method
+const init = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Ask a coding question:",
+      },
+    ])
+    .then((inquirerResponse) => {
+      promptFunc(inquirerResponse.name);
+    });
+};
+
+// Calls the initialization function and starts the script
+init();
